573531fce0218b0f0a1f52a01fd977a1facecb2b
redtroy <redtroy@126.com>
2014-07-25 09:57:44 +0800

    update

---
+++ 技术资料/私享家监管平台数据标准.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1406171425296" ID="ID_1648696341" MODIFIED="1406171441753" TEXT="&#x79c1;&#x4eab;&#x5bb6;&#x76d1;&#x7ba1;&#x5e73;&#x53f0;&#x6570;&#x636e;&#x6807;&#x51c6;">
+<node CREATED="1406171443957" ID="ID_1169212193" MODIFIED="1406171451277" POSITION="right" TEXT="&#x5e73;&#x53f0;&#x5185;&#x6570;&#x636e;&#x6807;&#x51c6;">
+<node CREATED="1406171452845" ID="ID_961018736" MODIFIED="1406171456632" TEXT="&#x5408;&#x540c;&#x5907;&#x6848;&#x53f7;"/>
+</node>
+</node>
+</map>
