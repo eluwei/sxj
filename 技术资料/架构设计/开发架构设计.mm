83584fa7bcdf69d61fcd48a4c38eda973f629505
redtroy <redtroyzhang@gmail.com>
2014-07-30 16:11:04 +0800

    Signed-off-by: redtroy <redtroyzhang@gmail.com>

---
+++ 技术资料/架构设计/开发架构设计.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1406706471683" ID="ID_1061555395" MODIFIED="1406706480717" TEXT="&#x5f00;&#x53d1;&#x67b6;&#x6784;&#x8bbe;&#x8ba1;">
+<node CREATED="1406706483289" ID="ID_233542853" MODIFIED="1406706489774" POSITION="right" TEXT="&#x57fa;&#x7ebf;&#x4ee3;&#x7801;&#x5e93;">
+<node CREATED="1406706491060" ID="ID_325946498" MODIFIED="1406706497889" TEXT="scm@192.168.1.10:/home/scm/repositories/springside4.git"/>
+</node>
+<node CREATED="1406706503018" ID="ID_438550858" MODIFIED="1406706575158" POSITION="right" TEXT="&#x5f00;&#x53d1;&#x4e3b;&#x5e72;">
+<node CREATED="1406706527307" ID="ID_7793371" MODIFIED="1406706538328" TEXT="scm@192.168.1.10:/home/scm/repositories/sxj-src.git"/>
+<node CREATED="1406706539030" ID="ID_499048258" MODIFIED="1406706545069" TEXT="scm@192.168.1.10:/home/scm/repositories/sxj-doc.git"/>
+</node>
+</node>
+</map>
