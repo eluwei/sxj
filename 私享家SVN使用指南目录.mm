94032bf6b18a45e3f6e24729748877018eb23f38
redtroy <redtroy@sxj.com>
2015-03-06 17:42:29 +0800

    Signed-off-by: redtroy <redtroy@sxj.com>

---
+++ 私享家SVN使用指南目录.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425456843305" ID="ID_590937694" MODIFIED="1425456859365" TEXT="&#x79c1;&#x4eab;&#x5bb6;SVN&#x4f7f;&#x7528;&#x6307;&#x5357;&#x76ee;&#x5f55;">
+<node CREATED="1425456860624" ID="ID_913413811" MODIFIED="1425456875670" POSITION="right" TEXT="&#x4e0b;&#x8f7d;&#x5b89;&#x88c5;">
+<node CREATED="1425456877443" ID="ID_356075610" MODIFIED="1425456889856" TEXT="&#x9010;&#x6b65;&#x622a;&#x56fe;&#x8bf4;&#x660e;"/>
+</node>
+<node CREATED="1425456891091" ID="ID_1526390951" MODIFIED="1425456918666" POSITION="right" TEXT="&#x5ba2;&#x6237;&#x7aef;&#x914d;&#x7f6e;"/>
+<node CREATED="1425456934539" ID="ID_59355468" MODIFIED="1425456943790" POSITION="right" TEXT="&#x4ece;&#x8fdc;&#x7a0b;&#x4ed3;&#x5e93;&#x66f4;&#x65b0;"/>
+<node CREATED="1425456943994" ID="ID_406381673" MODIFIED="1425456949064" POSITION="right" TEXT="&#x63d0;&#x4ea4;&#x5230;&#x8fdc;&#x7a0b;&#x4ed3;&#x5e93;"/>
+<node CREATED="1425456949287" ID="ID_1331145818" MODIFIED="1425456953339" POSITION="right" TEXT="&#x67e5;&#x770b;&#x5386;&#x53f2;&#x8bb0;&#x5f55;"/>
+</node>
+</map>
